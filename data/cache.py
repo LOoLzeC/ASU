5f34b4b9557d629990a77f6873943ad9f5fd7f6c
Deray <33969267+LOoLzeC@users.noreply.github.com>
2019-05-27 21:01:03 +0700

    Add files via upload

---
+++ data/cache.py
@@ -0,0 +1,13 @@
+#-*-coding:utf-8-*-
+# Coded By Deray
+'''
+	 Rebuild Copyright Can't make u real programmer
+'''
+# Report Bug On My Other Sosmed
+# instagram: @reyy05_
+# facebook: https://facebook.com/achmad.luthfi.hadi.3
+
+import os
+
+def cleanCache():
+	pass
