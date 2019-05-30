d5f98bf8c82a8a8261a8fe916147814dbbf70e3d
Deray <33969267+LOoLzeC@users.noreply.github.com>
2019-05-31 05:19:25 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-pkg install python2;pip2 install bs4;pip2 install requests;pip2 install mechanize;pkg install openssh;pkg install php;python2 asu.py
+pkg install python2;pip2 install bs4;pip2 install requests;pip2 install mechanize;pkg install openssh;pkg install php;python2 ASU.py
